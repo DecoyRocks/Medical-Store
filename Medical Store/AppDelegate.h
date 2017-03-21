86968d3aa4b4f72aaeba93176be1d32ddf8eb79c
Darshan Dixit <darshandixit@darshans-mac.local>
2017-03-21 17:34:06 +0530

    Initial Commit

---
+++ Medical Store/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Medical Store
+//
+//  Created by Darshan Dixit on 21/03/17.
+//  Copyright © 2017 Darshan Dixit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Medical Store/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Medical Store
+//
+//  Created by Darshan Dixit on 21/03/17.
+//  Copyright © 2017 Darshan Dixit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
